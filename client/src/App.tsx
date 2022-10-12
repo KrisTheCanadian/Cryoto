7e5936ec219e09b0d65c9d5139f356ba7fc759d5
Alexia Sousa <asousa@kpmglhx.onmicrosoft.com>
2022-10-12 03:50:03 +0000

    Merged PR 141: #440 - Language System
    ## Pull Request Description (proposed changes, what will be affected):
    i18n language system implemented for english and french
    
    ## Testing Instructions:
    1. Define translations in their respective language translation file
    2. Wrap your text like this: {t("text")}
    3. Pass in one of the following parameters in the URL: {?lng=en} or {?lng=fr}
    4. Watch the text change!
    
    ## Checklist:
    - [x] Your code builds clean without any errors/warnings
    - [x] You are using approved terminology/coding guidelines
    - [ ] You have added tests
    - [x] You did some QA/Validation of your changes
    
    #440 - Language System
    
    Related work items: #440

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,33 @@
 /* eslint-disable @shopify/jsx-no-hardcoded-content */
-import {useState} from 'react';
+import {Suspense, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const {t, i18n} = useTranslation();
 
   return (
-    <div className="App">
-      <>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </>
+    <Suspense fallback="Loading...">
+      <div className="App">
+        <>
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+        </>
 
-      <h1>Cryoto</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <h1>Cryoto</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Suspense>
   );
 }
 export default App;
